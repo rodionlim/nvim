a948f4091fd74936dfd57b470060350cdd379b7b
Rodion Lim <rodion.lim@hotmail.com>
2026-07-24 16:27:32 +0800

    feat: add clipboard copy and folding

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,6 +6,9 @@ vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
-vim.opt.splitright = true
-vim.opt.equalalways = false
 
+vim.opt.foldlevelstart = 99
+vim.opt.foldtext = ""
+
+
+vim.opt.clipboard:append("unnamedplus")
